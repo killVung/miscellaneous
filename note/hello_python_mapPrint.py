664caefb14a1fd56cfa94eaad40c196d149cbd37
killvung <kiulam125@gmail.com>
2016-10-02 23:00:09 -0500

    Add note

--- note/hello_python_mapPrint.py
+++ note/hello_python_mapPrint.py
@@ -2,5 +2,6 @@ N = int(input())
 '''
 Use map function to map the print function to eahc of the iterable element when python detects the s variable, it will automatically perform print function...
 This is a really interesting discovery, to print something without actually print something...
+Almost forgot, this doesn't work in Python2 because of the print function inside a map funciton, map returns a generator
 '''
 s = list(map(lambda fuck:print(fuck,end=""),range(1,N+1)))
