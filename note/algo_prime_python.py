09a33935a7688ed73a4429bf5e2d3cebad3871e8
killvung <kiulam125@gmail.com>
2016-10-03 15:39:56 -0500

    Use Decorator to check precondition before start doing prime

--- note/algo_prime_python.py
+++ note/algo_prime_python.py
@@ -2,12 +2,17 @@
 Write a program to check whether the number "n" is a prime number
 '''
 from math import sqrt
-def isPrime(n):
-    if n == 2:
-        return True
-    elif n >= 2 and n % 2 == 0:
-        return False
+def precondition(f):
+    def precondition_prime(n):
+        if n == 2:
+            return True
+        elif n >= 2 and n % 2 == 0:
+            return False
+        return f(n)
+    return precondition_prime
 
+@precondition
+def isPrime(n):
     for i in range(3,int(sqrt(n)) + 1, 2):
         if n % i == 0:
             return False
